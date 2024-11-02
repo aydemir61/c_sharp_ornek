7e3914674eb1c5f033dbc7b422eb753d90fa743d
Abdullah Aydemir <aabdullahaydemirmail.com>
2024-11-02 21:58:09 +0300

    ikinci hata ornek programi

---
+++ exception_exercises_2/exception_2/Program.cs
@@ -0,0 +1,25 @@
+﻿static void parola_kontrol(string password){
+    if(password.Length<=6||password.Length>=10){
+        throw new Exception("parola 6-10 karakter aralığında olmalıdır.");
+    }
+    if(!password.Any(char.IsDigit)){
+         throw new Exception("parola en az bir rakam içermelidir");
+    }
+    if(!password.Any(char.IsLetter)){
+         throw new Exception("parola en az bir harf içermelidir");
+    }
+}
+
+Console.Write("parola: ");
+string parola = Console.ReadLine();
+
+try
+{
+    parola_kontrol(parola);
+    Console.WriteLine("Parola Geçerli");
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine(e.Message);
+}
