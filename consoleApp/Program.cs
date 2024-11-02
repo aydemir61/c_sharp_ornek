13d4c19ce4e517b85a23cf220cbf2f6292facccd
Abdullah Aydemir <aabdullahaydemirmail.com>
2024-11-02 19:29:56 +0300

    urunlerin sonuna ekleme kodueklendi.

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -13,11 +13,12 @@
         urunler.Add(new Product(){Id=1,Title="IPhone 14",Price=40000});
         urunler.Add(new Product(){Id=1,Title="IPhone 15",Price=50000});
         urunler.Add(new Product(){Id=1,Title="IPhone 16",Price=60000});
-
+        urunler.Insert(urunler.Count, new Product(){Id=4, Title="IPhone 17", Price=70000});
         foreach (var urun in urunler)
         {
             Console.WriteLine(urun.Title+" "+urun.Price);
         }
+        
     }
 }
 class Product
