05e8a104925f9106eb65d948fca2c5f8f10414d2
Abdullah Aydemir <aabdullahaydemirmail.com>
2024-11-02 19:00:08 +0300

    liste ile ile ilgili denemeler yapıldı.

---
+++ generic_list.cs
@@ -0,0 +1,30 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> sayilar = new List<int>();
+        sayilar.Add(10);
+        sayilar.Add(20);
+        
+        List<string> isimler = new List<string>(){"ali","ahmet","ayşe"}; 
+
+        List<Product> urunler = new List<Product>();
+
+        urunler.Add(new Product(){Id=1,Title="IPhone 14",Price=40000});
+        urunler.Add(new Product(){Id=1,Title="IPhone 15",Price=50000});
+        urunler.Add(new Product(){Id=1,Title="IPhone 16",Price=60000});
+
+        foreach (var urun in urunler)
+        {
+            Console.WriteLine(urun.Title+" "+urun.Price);
+        }
+    }
+}
+class Product
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+    public double Price { get; set; }
+
+}
